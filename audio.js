f64def9e993f6dbcd437acc62a8f6475b2bc4f28
MirThara <mi.tha.14@icloud.com>
2025-12-10 18:57:29 +0100

    updated some assets

--- audio.js
+++ audio.js
@@ -13,30 +13,30 @@ function isDateReached(month, day) {
 }
 
 window.sounds = [
-    new Audio("https://mirthara.github.io/Adventskalender2025/assets/audio/OogwaysAscends.mp3"),
-    new Audio("https://mirthara.github.io/Adventskalender2025/assets/audio/WeWereTheLuckyOnes.mp3"),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/NeverForget.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/FrenchLibrary.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/ExperienceFlowsInYou.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/ComptineD\'unAutreEte.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Mondscheinsonate.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/AnOldFriend.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Fly.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/UnaMattina.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Gladiator.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/LondonCalling.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Lullaby.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/HijoDeLaLuna.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/WhoYouReallyAre.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/1Gymnopedie.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/1Gnossienne.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/PiratesOfTheCaribbean.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/LaPetiteFilleDeLaMer.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Picard.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Time.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/GoldenHour.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/VideoGames.mp3'),
-    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Interstellar.mp3')
+    new Audio("https://mirthara.github.io/Adventskalender2025/assets/audio/OogwaysAscends.mp3"), // 1
+    new Audio("https://mirthara.github.io/Adventskalender2025/assets/audio/WeWereTheLuckyOnes.mp3"), // 2
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/NeverForget.mp3'), // 3
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/FrenchLibrary.mp3'), // 4
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/ExperienceFlowsInYou.mp3'), // 5
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/ComptineD\'unAutreEte.mp3'), // 6
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Mondscheinsonate.mp3'), // 7
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/AnOldFriend.mp3'), // 8
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Fly.mp3'), // 9
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/UnaMattina.mp3'), // 10
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Gladiator.mp3'), // 11
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/LondonCalling.mp3'), // 12
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Lullaby.mp3'), // 13
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Scherzo.mp3'), // 14
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/HijoDeLaLuna.mp3'), // 15
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/WhoYouReallyAre.mp3'), // 16
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Pirates.mp3'), // 17
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/1Gnossienne.mp3'), // 18
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/LaPetiteFilleDeLaMer.mp3'), // 19
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Picard.mp3'), // 20
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Time.mp3'), // 21
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/GoldenHour.mp3'), // 22
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/VideoGames.mp3'), // 23
+    new Audio('https://mirthara.github.io/Adventskalender2025/assets/audio/Interstellar.mp3') // 24
 ];
 
 function stopAll() {
